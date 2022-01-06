5192fc30140cc2847e85b5e9394c49e15ab1e05e
msjones3 <cheryl-jones@slasmail.com>
2022-01-06 16:54:22 +1000

    complete validation rules

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import veeValidate from './includes/validation';
 import './assets/tailwind.css';
 import './assets/main.css';
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(veeValidate);
 app.mount('#app');
