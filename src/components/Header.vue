7bbb1645b58b0600a9992d025b97299397530949
msjones3 <cheryl-jones@slasmail.com>
2022-01-09 08:59:53 +1000

    uploader component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,19 +3,25 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4"
+      :to="{name: 'home' }" exact-active-class="no-active">
+        Music
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+          </li>
           <li v-if="!userLoggedIn">
             <a class="px-2 text-white" href="#"
             @click.prevent="toggleAuthModal()">Login / Register</a>
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{name: 'manage' }">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="signout">Logout</a>
@@ -28,7 +34,7 @@
 </template>
 
 <script>
-import { mapMutations, mapState, mapActions } from 'vuex';
+import { mapMutations, mapState } from 'vuex';
 
 export default {
   name: 'Header',
@@ -37,7 +43,13 @@ export default {
   },
   methods: {
     ...mapMutations(['toggleAuthModal']),
-    ...mapActions(['signout']),
+    signout() {
+      this.$store.dispatch('signout');
+      // console.log(this.$route);
+      if (this.$route.meta.requiresAuth) {
+        this.$router.push({ name: 'home' });
+      }
+    },
 
     // toggleAuthModal() {
     //   this.$store.commit('toggleAuthModal');
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,65 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
+import store from '@/store';
 
 const routes = [
   {
+    name: 'home',
     path: '/',
-    name: 'Home',
     component: Home,
   },
   {
+    name: 'about',
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
+  },
+  {
+    name: 'manage',
+    // alias is like a redirect, but the url appears to still be 'manage'
+    // it is better for search engines to use the other method (redirect)
+    // alias: '/manage',
+    path: '/manage-music',
+    component: Manage,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    // by adding a redirect we don't have to worry about
+    // users who have bookmarked the old url
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    // catchall will be checked last so that real routes are prioritized
+    // this would be a good place to redirect to 404
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  // matched references a list of paths the user is visiting
+  // you can use this to see if
+  // no need to go any further if the path does not require auth
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+  if (store.state.userLoggedIn) {
+    next();
+  // if auth required, and user not logged in redirect to home
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
