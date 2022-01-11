3b31073a80580b369ee4b553d584e4e6217a59a3
msjones3 <cheryl@realsocial.com.au>
2022-01-12 08:49:43 +1000

    update songs list on update

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
-<app-upload ref="upload" />
+<app-upload ref="upload" :addSong="addSong" />
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
           <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
@@ -41,14 +41,11 @@ export default {
   },
   async created() {
     // query the database
-    const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get();
-    snapshot.forEach((document) => {
-      const song = {
-        ...document.data(),
-        docID: document.id,
-      };
-      this.songs.push(song);
-    });
+    const snapshot = await songsCollection
+      .where('uid', '==', auth.currentUser.uid)
+      .get();
+
+    snapshot.forEach(this.addSong);
   },
   beforeRouteLeave(to, from, next) {
     this.$refs.upload.cancelUploads();
@@ -61,6 +58,13 @@ export default {
     },
     removeSong(i) {
       this.songs.splice(i, 1);
+    },
+    addSong(document) {
+      const song = {
+        ...document.data(),
+        docID: document.id,
+      };
+      this.songs.push(song);
     },
   },
   // beforeRouteEnter(to, from, next) {
