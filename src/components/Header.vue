9579a4d9d88b4d443ad54cd33d20ef97bec0997f
msjones3 <cheryl@realsocial.com.au>
2022-01-16 12:41:39 +1000

    update menu to switch between French and English

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,6 +28,13 @@
             </li>
           </template>
         </ul>
+        <ul class="flex flex-row mt-1 ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#"
+            @click.prevent="changeLocale">
+            {{ currentLocale }}</a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -40,6 +47,9 @@ export default {
   name: 'Header',
   computed: {
     ...mapState(['userLoggedIn']),
+    currentLocale() {
+      return this.$i18n.locale === 'fr' ? 'French' : 'English';
+    },
   },
   methods: {
     ...mapMutations(['toggleAuthModal']),
@@ -49,6 +59,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: 'home' });
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === 'fr' ? 'en' : 'fr';
     },
 
     // toggleAuthModal() {
