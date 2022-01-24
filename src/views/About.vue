392f6b1e5267b1fa74b0be988566825756efd43f
msjones3 <cheryl@realsocial.com.au>
2022-01-24 21:03:10 +1000

    register module only when needed

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,12 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import dummyModule from '@/store/modules/dummy';
+
+export default {
+  created() {
+    this.$store.registerModule('dummy', dummyModule);
+  },
+};
+</script>
